de0cd5ee6bbb48db71545b15f7bbf09953e01114
WILMES <maxencewilmes@gmail.com>
2019-06-13 14:43:58 +0200

    Revert "Update working"
    This reverts commit f6bc639d9c5bbeb1256be985b790e4dc5f8ac8f7.

--- ressources/PHP/create.php
+++ ressources/PHP/create.php
@@ -9,11 +9,11 @@ $sql = $conn->prepare("INSERT INTO vg_general (titre, prix, date_de_sortie, genr
 $sql->bindValue(":jeux", $jeux);
 // Vérifie si $prix est pas vide car la Database attend un float comme valeur
 if (!empty($prix)) {
-	$sql->bindValue(":prix", $prix);
+    $sql->bindValue(":prix", $prix);
 }
 // Si $prix est vide mettre 0 pour bien définir que c'est un nombre est non un chaine vide sinon conflit
 else {
-	$sql->bindValue(":prix", 0);
+    $sql->bindValue(":prix", 0);
 }
 $sql->bindValue(":release", $release);
 $sql->bindValue(":genre", $genre);
@@ -22,26 +22,18 @@ $sql->bindValue(":mode", $mode);
 $sql->bindValue(":connexion", $connexion);
 // Verifie si variable $plateforme est différent de vide sinon ça provoque un conflit avec la méthode implode
 if (!empty($plateforme)) {
-	$sql->bindValue(":plateforme", implode("/", $plateforme));
+    $sql->bindValue(":plateforme", implode("/", $plateforme));
 }
 // Si $plateforme est vide alors ce n'est pas un tableau donc pas besoin de la méthode implode
 else {
-	$sql->bindValue(":plateforme", $plateforme);
+    $sql->bindValue(":plateforme", $plateforme);
 }
 $sql->bindValue(":description", $description);
 
+$request = $sql->execute();
 
-if (empty($jeux) || (empty($release))) {	
-	header('Location:../../index.php?update=empty');
+if ($request === true){
+    header('Location:../../index.php?refresh=ok');
 } else {
-	$request = $sql->execute();	
-	if ($request === true){
-		header('Location:../../index.php?update=refresh');
-	} else {
-		echo "ERROR : ";
-	}
+    echo "ERROR : ";
 }
-
-
-	
-	
